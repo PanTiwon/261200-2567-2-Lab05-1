76059d1e7cb8dfb6f1bf6a6d7e09b9670cfaeff9
PanTiwon <kkhowphuns@gmail.com>
2026-01-09 15:41:20 +0700

    Update

---
+++ User.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+
+public class User {
+    protected String name;
+    protected LocalDate dob;
+
+    public User() {
+        this.name = null;
+        this.dob = LocalDate.now();
+    }
+
+    public User(String name, int year, int month, int day) {
+        this.name = name;
+        this.dob = LocalDate.of(year, month, day);
+    }
+    public String getName() {
+        return name;
+    }
+    public LocalDate getDob() {
+        return dob;
+    }
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Birthday: " + dob);
+    }
+}
